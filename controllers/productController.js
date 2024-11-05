5456d8bdc15107fa2c79db2d9f06ff7452c70aff
Frank Njoroge <fnjoro29@gmail.com>
2024-11-05 12:34:08 +0300

    add functionality to handle POST requests using static data

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,7 @@
 const Product = require("../models/productModel");
 
 // @desc   Gets all products
-// @route  /api/products
+// @route  GET /api/products
 async function getProducts(req, res) {
   try {
     const products = await Product.findAll();
@@ -15,7 +15,7 @@ async function getProducts(req, res) {
 }
 
 // @desc   Gets single product
-// @route  /api/product/:id
+// @route GET /api/product/:id
 async function getProduct(req, res, id) {
   try {
     const product = await Product.findById(id);
@@ -35,7 +35,27 @@ async function getProduct(req, res, id) {
   }
 }
 
+// @desc   Add a new product
+// @route POST /api/products
+async function createProduct(req, res) {
+  try {
+    const product = {
+      title: "Test Product",
+      description: "This is my new product",
+      price: 100,
+    };
+    const newProduct = await Product.create(product);
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    res.end(JSON.stringify(newProduct));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 module.exports = {
   getProducts,
   getProduct,
+  createProduct,
 };
